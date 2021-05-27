a2257f96bc9ac2240ba9b918d741b6f273a6f1cc
Davy-Dray <84913576+Davy-Dray@users.noreply.github.com>
2021-05-27 15:19:26 +0100

    Add files via upload

---
+++ Completed/Assessment.sql
@@ -0,0 +1,14 @@
+
+
+
+
+/*Assessment Table*/
+
+create table Assessment(Assessment_Id int primary key,
+                       Assesment_Type varchar(10),
+					   Term varchar(10),
+					   [DATE] DATE
+					   )
+insert into Assessment values(1,'Test','1st')
+insert into Assessment values(2,'Exam','1st')
+
